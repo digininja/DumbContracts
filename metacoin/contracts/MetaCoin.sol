dfee44f3d51c01bb4f4e01241e7f421d0e615869
Robin <robin@digi.ninja>
2023-01-06 22:25:26 +0000

    package working

---
+++ metacoin/contracts/MetaCoin.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+// Tells the Solidity compiler to compile only from v0.8.13 to v0.9.0
+pragma solidity ^0.8.13;
+
+import "./ConvertLib.sol";
+
+// This is just a simple example of a coin-like contract.
+// It is not ERC20 compatible and cannot be expected to talk to other
+// coin/token contracts.
+
+contract MetaCoin {
+	mapping (address => uint) balances;
+	mapping (uint => uint) mappings;
+
+	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+	constructor() {
+		balances[tx.origin] = 10000;
+		mappings[23] = 6;
+		mappings[26] = 3;
+	}
+
+	function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
+		if (balances[msg.sender] < amount) return false;
+		balances[msg.sender] -= amount;
+		balances[receiver] += amount;
+		emit Transfer(msg.sender, receiver, amount);
+		return true;
+	}
+
+	function getBalanceInEth(address addr) public view returns(uint){
+		return ConvertLib.convert(getBalance(addr),2);
+	}
+
+	function getBalance(address addr) public view returns(uint) {
+		return balances[addr];
+	}
+
+	function getArbMapping(uint key) public view returns(uint) {
+		// If a key does not exist an empty element is returned.
+		// No error is thrown.
+		return mappings[key];
+	}
+}
