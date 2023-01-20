7143853edb37bbe53aa93d26d0c026de0d7640d3
Robin <robin@digi.ninja>
2023-01-20 16:27:05 +0000

    starting with custom errors

--- calling/contracts/calling.sol
+++ calling/contracts/calling.sol
@@ -10,10 +10,15 @@ contract CallMe {
     uint public a = 1;
     uint public b = 2;
 
+	// Custom errors
+	// https://blog.soliditylang.org/2021/04/21/custom-errors/
+	// https://medium.com/coinmonks/solidity-revert-with-custom-error-explained-with-example-d9dff8937ef4
+	error NotEnoughGiven(string message, uint value);
+
     function setB(uint _b) public returns (uint) {
         b = _b;
         if (b < 100) {
-            revert("Not enough, I want more");
+            revert NotEnoughGiven("Not enough, I want more", _b);
         }
         return b;
     }
